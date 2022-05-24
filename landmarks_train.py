c7963383ef4a6cfbb412d24503732fd89b1b5256
CarlosCues <97036885+CarlosCues@users.noreply.github.com>
2022-05-24 20:20:02 +0200

    Delete landmarks_train.py

--- landmarks_train.py
+++
@@ -1,9 +0,0 @@
-from extract_landmarks import extract_landmarks
-from os import listdir
-import cv2
-
-basepath=[f'train/{imagen}' for imagen in listdir('train') ]
-
-landmarks_train=map(extract_landmarks,basepath[:3])
-landmarks_train=list(landmarks_train)
-print(landmarks_train)
